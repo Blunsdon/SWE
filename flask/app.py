0618bb94b4c6219ca285cd77f05f388df1b56127
Blunsdon <blunsdon@hotmail.dk>
2019-12-11 15:26:14 +0100

    Final workes on Windows

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+
+# Passes the name app as main
 app = Flask(__name__)
 
+# Sets up route with message
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return 'Flask Dockerized anything good'
 
+# if OK run lokalhost
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
